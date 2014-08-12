4b4ecae5481aa9bd0bfd1f56fec285e6beb58a33
Matej Ramuta <matej.ramuta@gmail.com>
2014-08-12 10:19:01 +0200

    new version

---
+++ app/src/androidTest/java/fass2014c/ramuta/me/fass2014/ApplicationTest.java
@@ -0,0 +1,13 @@
+package fass2014c.ramuta.me.fass2014;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
