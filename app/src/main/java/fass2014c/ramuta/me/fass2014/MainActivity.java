c982645ed6358bb27530d6adbe6067dc22ce69c9
ekranac <besal.ziga@gmail.com>
2014-08-12 11:08:25 +0200

    fragments fix

--- app/src/main/java/fass2014c/ramuta/me/fass2014/MainActivity.java
+++ app/src/main/java/fass2014c/ramuta/me/fass2014/MainActivity.java
@@ -5,9 +5,11 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -51,9 +53,25 @@ public class MainActivity extends Activity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Log.i("Main",Integer.toString(position));
+
+
+        if(position==1)
+        {
+            ExampleFragment fragment = new ExampleFragment();
+            fragmentTransaction.replace(R.id.container,fragment);
+
+        }
+        else
+        {
+            ProgramFragment fragment = new ProgramFragment();
+            fragmentTransaction.replace(R.id.container,fragment);
+
+        }
+
+        fragmentTransaction.commit();
     }
 
     public void onSectionAttached(int number) {
@@ -97,10 +115,26 @@ public class MainActivity extends Activity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class ExampleFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+    public static class ProgramFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_program, container, false);
+        }
     }
 
     /**
